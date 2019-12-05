950f229cb010c0831bc1a5e15549057a1933ddcb
Ravi Dhiman <ravi.dhiman@tokopedia.com>
2019-12-05 18:58:10 +0530

    Updates

---
+++ precache-manifest.8e56056020c2ff69c3612e03be494396.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "561b6a4c173f1f8f6057",
+    "url": "/shiny-spoon/static/js/runtime~main.561b6a4c.js"
+  },
+  {
+    "revision": "4f8fcd8221e46565360c",
+    "url": "/shiny-spoon/static/js/main.4f8fcd82.chunk.js"
+  },
+  {
+    "revision": "c6eb8a3e1750d56f8cc8",
+    "url": "/shiny-spoon/static/js/1.c6eb8a3e.chunk.js"
+  },
+  {
+    "revision": "4f8fcd8221e46565360c",
+    "url": "/shiny-spoon/static/css/main.c9b660b9.chunk.css"
+  },
+  {
+    "revision": "fbbca5f5fdbe6c5dd4e87edea8b6eda3",
+    "url": "/shiny-spoon/index.html"
+  }
+];
